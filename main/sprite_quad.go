e4a509d1809a234a20035c378ada8a594074b172
Artsiom Trubchyk <aglitchman@gmail.com>
2023-10-12 20:04:37 +0300

    Add models as sprites

---
+++ main/sprite_quad.go
@@ -0,0 +1,15 @@
+components {
+  id: "sprite_quad"
+  component: "/main/sprite_quad.model"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
